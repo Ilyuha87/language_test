776d84fe2433b63af05c4cbb2b99c7ddd54d7270
Ilyuha87 <105043678+Ilyuha87@users.noreply.github.com>
2022-08-09 20:24:30 +0300

    files for lesson added

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+
+def test_button_add_to_basket_exist_on_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
+    print("страница товара на сайте содержит кнопку добавления в корзину")
+
+
